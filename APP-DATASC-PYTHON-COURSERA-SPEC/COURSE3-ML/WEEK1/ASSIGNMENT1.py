c7bcddf27bf7cad0770d0ea8fe8316550afc181f
Samrat Kar <samratk@gmail.com>
2018-09-02 22:04:22 +0530

    question 1 completed

--- APP-DATASC-PYTHON-COURSERA-SPEC/COURSE3-ML/WEEK1/ASSIGNMENT1.py
+++ APP-DATASC-PYTHON-COURSERA-SPEC/COURSE3-ML/WEEK1/ASSIGNMENT1.py
@@ -8,3 +8,53 @@ from sklearn.datasets import load_breast_cancer
 cancer = load_breast_cancer()
 print(cancer.DESCR) # Print the data set description
 cancer.keys()
+cancer['feature_names']
+
+'''
+Question 0 (Example)
+#How many features does the breast cancer dataset have?
+#This function should return an integer.
+# You should write your whole answer within the function provided. The autograder will call
+# this function and compare the return value against the correct solution value
+'''
+def answer_zero():
+    # This function returns the number of features of the breast cancer dataset, which is an integer.
+    # The assignment question description will tell you the general format the autograder is expecting
+    return len(cancer['feature_names'])
+
+# You can examine what your function returns by calling it in the cell. If you have questions
+# about the assignment formats, check out the discussion forums for any FAQs
+answer_zero()
+
+'''
+Question 1
+Scikit-learn works with lists, numpy arrays, scipy-sparse matrices, and pandas DataFrames, so converting the dataset to a DataFrame is not necessary for training this model. Using a DataFrame does however help make many things easier such as munging data, so let's practice creating a classifier with a pandas DataFrame.
+
+Convert the sklearn.dataset cancer to a DataFrame.
+This function should return a (569, 31) DataFrame with
+
+columns =
+
+['mean radius', 'mean texture', 'mean perimeter', 'mean area',
+'mean smoothness', 'mean compactness', 'mean concavity',
+'mean concave points', 'mean symmetry', 'mean fractal dimension',
+'radius error', 'texture error', 'perimeter error', 'area error',
+'smoothness error', 'compactness error', 'concavity error',
+'concave points error', 'symmetry error', 'fractal dimension error',
+'worst radius', 'worst texture', 'worst perimeter', 'worst area',
+'worst smoothness', 'worst compactness', 'worst concavity',
+'worst concave points', 'worst symmetry', 'worst fractal dimension',
+'target']
+and index =
+
+RangeIndex(start=0, stop=569, step=1)
+'''
+df = pd.DataFrame(cancer['data'], columns = cancer['feature_names'])
+df['target'] = cancer['target']
+df.head()
+'''
+Question 2
+What is the class distribution? (i.e. how many instances of malignant (encoded 0) and how many benign (encoded 1)?)
+
+This function should return a Series named target of length 2 with integer values and index = ['malignant', 'benign']
+'''

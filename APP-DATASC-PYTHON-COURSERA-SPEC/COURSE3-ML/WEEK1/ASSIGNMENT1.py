f022da9ea52c402aa508f4ed7ce020f197a4c3c2
Samrat Kar <samratk@gmail.com>
2018-09-03 20:42:30 +0530

    correction on assignment - 88%

--- APP-DATASC-PYTHON-COURSERA-SPEC/COURSE3-ML/WEEK1/ASSIGNMENT1.py
+++ APP-DATASC-PYTHON-COURSERA-SPEC/COURSE3-ML/WEEK1/ASSIGNMENT1.py
@@ -49,9 +49,12 @@ and index =
 
 RangeIndex(start=0, stop=569, step=1)
 '''
-df = pd.DataFrame(cancer['data'], columns = cancer['feature_names'])
-df['target'] = cancer['target']
-df.head()
+def answer_one():
+    df = pd.DataFrame(cancer['data'], columns = cancer['feature_names'])
+    df['target'] = cancer['target']
+    df.head()
+    df.shape
+    return df
 '''
 Question 2
 What is the class distribution? (i.e. how many instances of malignant (encoded 0) and how many benign (encoded 1)?)
@@ -77,5 +80,89 @@ This function should return a tuple of length 2: (X, y), where
 X, a pandas DataFrame, has shape (569, 30)
 y, a pandas Series, has shape (569,).
 '''
+def answer_three():
+    X = df.iloc[:,:-1]
+    y = df.iloc[:,30]
+    return (X,y)
 
-    
+'''
+Question 4
+Using train_test_split, split X and y into training and test sets (X_train, X_test, y_train, and y_test).
+
+Set the random number generator state to 0 using random_state=0 to make sure your results match the autograder!
+
+This function should return a tuple of length 4: (X_train, X_test, y_train, y_test), where
+
+X_train has shape (426, 30)
+X_test has shape (143, 30)
+y_train has shape (426,)
+y_test has shape (143,)
+'''
+from sklearn.model_selection import train_test_split
+def answer_four():
+    X, y = answer_three()
+    X_train, X_test, y_train,  y_test = train_test_split(X,y,random_state=0)
+    X_train.shape
+    X_test.shape
+    y_train.shape
+    y_test.shape
+    return [X_train, X_test, y_train, y_test]
+
+''''
+Question 5
+Using KNeighborsClassifier, fit a k-nearest neighbors (knn) classifier with X_train, y_train and using one nearest neighbor (n_neighbors = 1).
+
+This function should return a  sklearn.neighbors.classification.KNeighborsClassifier.
+'''
+from sklearn.neighbors import KNeighborsClassifier
+​
+def answer_five():
+    X_train, X_test, y_train, y_test = answer_four()
+
+    knn = KNeighborsClassifier(n_neighbors=1)
+    return knn.fit(X_train, y_train)
+'''
+Question 6
+Using your knn classifier, predict the class label using the mean value for each feature.
+
+Hint: You can use cancerdf.mean()[:-1].values.reshape(1, -1) which gets the mean value for each feature, ignores the target column, and reshapes the data from 1 dimension to 2 (necessary for the precict method of KNeighborsClassifier).
+
+This function should return a numpy array either array([ 0.]) or array([ 1.])
+'''
+
+def answer_six():
+    cancerdf = answer_one()
+    means = cancerdf.mean()[:-1].values.reshape(1, -1)
+    knn = answer_five()
+
+    return knn.predict(means)
+
+answer_six()
+'''
+Question 7
+Using your knn classifier, predict the class labels for the test set X_test.
+
+This function should return a numpy array with shape (143,) and values either 0.0 or 1.0.
+'''
+
+def answer_seven():
+    X_train, X_test, y_train, y_test = answer_four()
+    knn = answer_five()
+
+    return knn.predict(X_test)
+​
+answer_seven()
+'''
+Question 8
+Find the score (mean accuracy) of your knn classifier using X_test and y_test.
+
+This function should return a float between 0 and 1
+'''
+
+def answer_eight():
+    X_train, X_test, y_train, y_test = answer_four()
+    knn = answer_five()
+
+    return knn.score(X_test, y_test)
+
+answer_eight()

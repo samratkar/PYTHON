e161dd33cdfc255173b5e90a61b374db7d46c7e1
Samrat Kar <samratk@gmail.com>
2018-09-10 21:29:39 +0530

    ARANGE

--- BASICS/TRY.py
+++ BASICS/TRY.py
@@ -1,7 +1,10 @@
 #Unlike C, python does not convert automatically the character to ascii number.
+import numpy as np
 try:
     data = input ("Enter name")
     numeric_data = int(data)
 except:
     numeric_data = -1
 print (numeric_data)
+
+np.arange(5)

c1439833221566597f039b985fa1c8d3855145ae
Samrat Kar <samratk@gmail.com>
2018-09-13 22:33:24 +0530

    initial dev

---
+++ APP-DATASC-PYTHON-COURSERA-SPEC/COURSE3-ML/WEEK2/DATASETS.py
@@ -0,0 +1,63 @@
+import os
+path="/home/samrat/Documents/Git/PYTHON/APP-DATASC-PYTHON-COURSERA-SPEC/COURSE3-ML/WEEK2"
+os.chdir(path)
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_classification, make_blobs
+from matplotlib.colors import ListedColormap
+from sklearn.datasets import load_breast_cancer
+from adspy_shared_utilities import load_crime_dataset
+
+cmap_bold = ListedColormap(['#FFFF00', '#00FF00', '#0000FF','#000000'])
+
+
+# synthetic dataset for simple regression
+from sklearn.datasets import make_regression
+#plt.figure()
+plt.title('Sample regression problem with one input variable')
+X_R1, y_R1 = make_regression(n_samples = 100, n_features=1,
+                            n_informative=1, bias = 150.0,
+                            noise = 30, random_state=0)
+
+plt.scatter(X_R1, y_R1, marker= 'o', s=50)
+plt.show()
+# synthetic dataset for more complex regression
+from sklearn.datasets import make_friedman1
+plt.figure()
+plt.title('Complex regression problem with one input variable')
+X_F1, y_F1 = make_friedman1(n_samples = 100,
+                           n_features = 7, random_state=0)
+
+plt.scatter(X_F1[:, 2], y_F1, marker= 'o', s=50)
+plt.show()
+
+# synthetic dataset for classification (binary)
+plt.figure()
+plt.title('Sample binary classification problem with two informative features')
+X_C2, y_C2 = make_classification(n_samples = 100, n_features=2,
+                                n_redundant=0, n_informative=2,
+                                n_clusters_per_class=1, flip_y = 0.1,
+                                class_sep = 0.5, random_state=0)
+plt.scatter(X_C2[:, 0], X_C2[:, 1], c=y_C2,
+           marker= 'o', s=50, cmap=cmap_bold)
+plt.show()
+
+
+# more difficult synthetic dataset for classification (binary)
+# with classes that are not linearly separable
+X_D2, y_D2 = make_blobs(n_samples = 100, n_features = 2, centers = 8,
+                       cluster_std = 1.3, random_state = 4)
+y_D2 = y_D2 % 2
+plt.figure()
+plt.title('Sample binary classification problem with non-linearly separable classes')
+plt.scatter(X_D2[:,0], X_D2[:,1], c=y_D2,
+           marker= 'o', s=50, cmap=cmap_bold)
+plt.show()
+
+
+# Breast cancer dataset for classification
+cancer = load_breast_cancer()
+(X_cancer, y_cancer) = load_breast_cancer(return_X_y = True)
+
+
+# Communities and Crime dataset
+(X_crime, y_crime) = load_crime_dataset()

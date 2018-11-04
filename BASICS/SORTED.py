f87d60baff363df94a0655086a6d7edaaf6d8a90
Samrat Kar <samratk@gmail.com>
2018-11-04 23:47:47 +0530

    update new way to use format with curly braces

--- BASICS/SORTED.py
+++ BASICS/SORTED.py
@@ -20,12 +20,20 @@ def key_fn(object):
 #    if object == 'r':
 #        return 91
 
-#a = 'Samrat Kar'
-#list_a = list(a)
+a = 'Samrat Kar'
+list_a = list(a)
+print (list_a)
+list_b = [9,8,7,6,5,4,3,2,1,0]
+list_b.sort()
+print (list_b)
+list_b = [9,8,7,6,5,4,3,2,1,0]
+sorted(list_b)
+
 #print (a)
 #print (list_a)
 #print (sorted(list_a, key=key_fn))
 #print (sorted(list_a))
 print (sorted([7,10,98,-4,0,5]))
+print (sorted(list_a))
 
 # NOTE: key is a function that takes one argument. This one argument is the element from the list of the first argument of the method sorted(). return value can be an transofrmation on the element.

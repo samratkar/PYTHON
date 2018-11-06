ba9093b2722d2717c12f117c23fcc7db22fee89c
Samrat Kar <samratk@gmail.com>
2018-11-06 23:06:13 +0530

    count all entries starting with s

--- BASICS/MAP.py
+++ BASICS/MAP.py
@@ -12,3 +12,17 @@ def split_title_and_name(person):
     return split_name[0]+split_name[-1]
 
 list(map(split_title_and_name, people))
+
+#NOTE : square the elements of a list
+my_list = [1,2,3,4,5,6]
+print (list(map(lambda x:x**2, my_list)))
+
+#NOTE : count all the entries which start with S
+import ast,sys
+input_str = sys.stdin.read()
+input_list = ast.literal_eval(input_str)
+s_list = list()
+count = 0
+my_list = list(map(lambda x: 1 if (x[0] == 'S') else 0, input_list))
+count = sum(my_list)
+print(count)

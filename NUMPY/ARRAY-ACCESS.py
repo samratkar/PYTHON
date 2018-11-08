8c61f6eac19009bab11ca3d4d13a90b384ac4efd
Samrat Kar <samratk@gmail.com>
2018-11-08 23:23:48 +0530

    IIIT B additions

--- NUMPY/ARRAY-ACCESS.py
+++ NUMPY/ARRAY-ACCESS.py
@@ -1,6 +1,8 @@
 import numpy as np
 array_1d = np.arange(1,20,2)
 print (array_1d)
+print (array_1d[4])
+print (array_1d[4,6])
 
 #accessing by providing a list of indices!
 print (array_1d[[2,3,4]])
@@ -16,10 +18,11 @@ IN NORMAL PYTHON LIST, YOU USE SQUARE BRACES TO ACCESS ONE ELEMENT FROM THE ARRA
 #From a 2D array extract all the rows of the 2 column.
 #Hint: 2 column will have index value as 1.
 
-import ast,sys
-input_str = sys.stdin.read()
-input_list = ast.literal_eval(input_str)
+input_list1 = [41,42,43,44,45,46,47,48,49]
+input_list2 = [51,52,53,54,55,56,57,58,59]
 import numpy as np
-array_2d =np.array(input_list)
+array_2d =np.array([input_list1, input_list2])
 
 print(array_2d[:,1])
+print (array_2d[0,3])
+print (array_2d[[0,1]])
--- PANDAS/SERIES.py
+++ PANDAS/SERIES.py
@@ -1,9 +1,11 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
 animals = ['Tiger', 'Bear', 'Moose']
 #the data type is written by default
-pd.Series(animals)
-animals 
+animals
+animals_series = pd.Series(animals)
+animals_series
+animals_series[[0,2]]
 numbers = [1,2,3,4]
 pd.Series(numbers)
 #Not a number - None in python is automatically converted to None for strings and Nan for numbres by pandas.
@@ -16,11 +18,11 @@ pd.Series(animals)
 sports = {'Archery': 'Bhutan', 'Golf': 'Scotland', 'Sumo':'Japan', 'Taekwando': 'South Korea'}
 s = pd.Series(sports)
 s.index
-#providing the index separately from data. 
+#providing the index separately from data.
 s = pd.Series(['Tiger','Bear','Moose'], index = ['India', 'Canada', 'Austrailia'])
 s
 
-#quering 
+#quering
 s.iloc[0]
 s[0]
 s.loc['India']
@@ -36,16 +38,15 @@ s.iloc[0]
 
 # working with data
 s = pd.Series([100,200,300,400])
-#vectorization of sum 
+#vectorization of sum
 s.sum()
 
 s = pd.Series(np.random.randint(0,1000,10))
 s.head
 
-
-
-
-
-
-
-
+# NOTE: apply()
+import pandas as pd
+series_1 = pd.Series([1,2,3,4])
+series_1
+series_1*2
+series_1**2

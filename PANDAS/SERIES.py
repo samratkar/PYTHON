248c1d5db1bd96637eba5f092d20a1c857abfd83
Samrat Kar <Samrat.Kar@honeywell.com>
2018-11-12 19:27:47 +0530

    updating index in series

--- PANDAS/SERIES.py
+++ PANDAS/SERIES.py
@@ -1,6 +1,7 @@
 '''
 NOTES -
-1. If dictionary is passed into the series as input, then the mapping between the values and the indices are maintained. This can be seen in the example in line# 26. This tight linkage between the index and the values are not maintained if the input to the series is a list as shown in the example above it.
+1. If dictionary is passed into the series as input, then the mapping between the values and the indices are maintained. This can be seen in the example in line# 26. This tight linkage between the index and the values are not maintained if the input to the series is a list as shown in the example above it. So, for the new index a new  entry is added, and the existing mapptings are not even distrubed. The entry for which indices does not occur in the new index list are omitted outright. This is a way to filter out data!!
+But if the input is just a list, then the mappings are disturbed and indices are updated in order of the entry of the new indices. 
 
 2. Since pandas are built on the top of the numpy array, all the broadcasting of operations (vectorization) is enabled by default with pandas. So, lambda functions and mapping them to the data strcutures are no more required.
 
@@ -23,6 +24,9 @@ animals = ['Tiger', 'Bear', 'Moose', 'Lion']
 pd.Series(animals)
 pd.Series(animals, index = ['a','b','c','d'])
 pd.Series(animals, index = ['a','x','c','d'])
+
+## NOTE : INDEX REHASHING WITH DICT INPUT
+import pandas as pd
 sdata = {'a':'Tiger', 'b':'Bear', 'c':'Moose', 'd': 'Lion'}
 obj3 = pd.Series(sdata)
 obj3
@@ -74,10 +78,11 @@ n = int(input())
 
 import numpy as np
 import pandas as pd
+n = 5
 data1 = np.array(range(1,n+1))
 series1 = pd.Series(data1**2, index = data1 )
 print(series1)
-
+series1 [1]
 
 # NOTE : set_index() sort_index()
 
@@ -85,3 +90,5 @@ df.set_index ('Order_index', inplace = True)
 df.sort_index(ascending = False )
 df.sort_values(by='Sales')
 df.sort_values(by=['Sales','Product_id'])
+
+

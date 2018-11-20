0e63407dcc785f8755eaf2f570401e15c2588ad2
Samrat Kar <samratk@gmail.com>
2018-11-20 23:24:26 +0530

    CLEANING

--- CLEANING/CLEANING.py
+++ CLEANING/CLEANING.py
@@ -36,3 +36,13 @@ df = pd.read_csv('https://query.data.world/s/Hfu_PsEuD1Z_yJHmGaxWTxvkz7W_b0')
 df = df[df.isnull().sum(axis=1) <= 5]
 
 print(round(100*(df.isnull().sum()/len(df.index)), 2))#Round off to 2 decimal places.
+
+##NOTE - Mean Imputation
+#Description
+#Impute the mean value at all the missing values of the column 'Product_Base_Margin' and then print the percentage of missing values in each column.
+
+import numpy as np
+import pandas as pd
+df = pd.read_csv('https://query.data.world/s/Hfu_PsEuD1Z_yJHmGaxWTxvkz7W_b0')
+df.loc[np.isnan(df['Product_Base_Margin']), ['Product_Base_Margin']] = df['Product_Base_Margin'].mean()
+print(round(100*(df.isnull().sum()/len(df.index)), 2))

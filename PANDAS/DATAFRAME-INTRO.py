d9b47f4cf0adb4a3569b67424cc26a54c87a48f4
Samrat Kar <samratk@gmail.com>
2018-11-21 22:28:35 +0530

    definition of dataframes

--- PANDAS/DATAFRAME-INTRO.py
+++ PANDAS/DATAFRAME-INTRO.py
@@ -1,3 +1,46 @@
+'''NOTE: WES MCKINNEY - PYTHON FOR DATA ANALYSIS
+1. A DataFrame represents a data structure containing an "ORDERED" collection of "COLUMNS", each of which can be a different value type (numeric,string, boolean, etc.)
+2. A DataFrame is a Dictionary of Series, where each series is a column.
+3. The values of each column is arranged in ascending order
+4. Under the hood, the data is stored as one or more two-dimensional blocks rather than a list, dict, or some other collection of one-dimensional arrays.
+5. While DataFrame stores the data internally in a two-dimensional for-mat, you can easily represent much higher-dimensional data in a tabular format using hierarchical indexing.
+
+'''
+#NOTE - EXAMPLE 1 - DATA FRAMES ARE ORDERED COLLECTION OF COLUMNS !!
+import pandas as pd
+data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'],
+      'year' : [2000, 2001, 2002, 2001, 2002],
+      'pop'  : [1.5, 1.7, 3.6, 2.4, 2.9]}
+df = pd.DataFrame(data)
+df
+# NOTE: - conditional indexing!
+df[[False, False, True, True, True]]
+df[pd.Series([False, False, True, True, True])]
+s1 = df['pop'] >2
+df[s1]
+#NOTE - EXAMPLE 2 - you can change the sequence of columns -
+df = pd.DataFrame(data, columns = ['year', 'pop', 'state'])
+df
+#NOTE - EXAMPLE 3 - you can add new columns simply by appending new ones. If you dont append, the data will be purged
+df = pd.DataFrame(data, columns = ['capital', 'minister', 'lit rate'])
+df
+df = pd.DataFrame(data, columns = ['state', 'year', 'pop', 'capital', 'minister', 'lit rate'])
+df
+#NOTE - EXAMPLE 4 - INDEXING - you can index the entire column directly in square braces as dataframes are ordered list of columns. Each column is a series that is printed entirely.
+df ['state']
+type(df['state'])
+
+#NOTE - EXAMPLE 5 - The rows are known as indices. You can specific the rows by providing the index names too!
+df = pd.DataFrame(data, index = ['zero', 'one', 'two', 'three', 'four'])
+df
+#NOTE - EXAMPLE 6 - For nested dictionaries outer index belong to the column names and inner indices are the row indices. Obviously! Since dataframes are collection of columns!
+pop = pd.DataFrame({'Nevada': {2001: 2.4, 2002: 2.9},'Ohio': {2000: 1.5, 2001: 1.7, 2002: 3.6}})
+pop
+
+
+
+# you can change the row idenx of the dataframe
+df = pd.DataFrame(data, index = ['Case1'])
 '''
 1. A dataframe is a table that contains a list of objects of same type!
 2. Every row corresponds to one object which has a numberical index. So the entire table is an agregation of multiple objects of the same type.

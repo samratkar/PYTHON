e1a98b60c7fb7e9069b45978a82740e0b2fc074e
Samrat Kar <samratk@gmail.com>
2018-11-25 23:34:56 +0530

    intro

--- MATPLOTLIB/Stat-plots.py
+++ MATPLOTLIB/Stat-plots.py
@@ -1,4 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-pd.read_csv("DATA-FILES/market_fact.csv")
+df = pd.read_csv("DATA-FILES/market_fact.csv")
 df.head()
+
+#NOTE - BOXPLOT
+plt.boxplot(df['Order_Quantity'])
+plt.boxplot(df['Sales'])
+df['Sales'].describe()
+
+#NOTE - HISTOGRAM
+plt.hist(df['Order_Quantity'])
+plt.hist(df['Sales'])
+plt.scatter(df['Sales'], df['Profit'])

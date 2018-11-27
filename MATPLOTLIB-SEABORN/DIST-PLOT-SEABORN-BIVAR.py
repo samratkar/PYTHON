b1d6c4e9e0fec3c703068add7d26988f84d03ca4
Samrat Kar <samratk@gmail.com>
2018-11-27 23:12:05 +0530

    subset of data frame visualization

--- MATPLOTLIB-SEABORN/DIST-PLOT-SEABORN-BIVAR.py
+++ MATPLOTLIB-SEABORN/DIST-PLOT-SEABORN-BIVAR.py
@@ -10,6 +10,16 @@ sns.jointplot('Sales', 'Profit', df)
 sns.jointplot(df['Sales'], df['Profit'])
 plt.show()
 
+#zooming in
+df1 = df[(df['Sales'] < 20000) & (df['Profit'] < 10000)]
+sns.jointplot(df1['Sales'], df1['Profit'])
+df = pd.read_csv('DATA-FILES/market_fact.csv')
+df1 = df[(df['Sales'] < 100) & (df['Profit'] > -100) & (df['Sales'] < 200)]
+sns.jointplot(df['Sales'], df['Profit'], kind = 'hex')
+plt.show()
+#HEAT MAP
 
 
+df = pd.read_csv('DATA-FILES/market_fact.csv')
 sns.pairplot(df)
+plt.show()

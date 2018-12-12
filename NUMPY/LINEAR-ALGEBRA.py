c7a9e8655188aaf0f01669ddc4a9ed38e4ac576a
Samrat Kar <samratk@gmail.com>
2018-12-12 23:37:44 +0530

    dot product application

--- NUMPY/LINEAR-ALGEBRA.py
+++ NUMPY/LINEAR-ALGEBRA.py
@@ -6,5 +6,42 @@ print (b)
 np.linalg.det(a)
 np.linalg.inv(a)
 np.linalg.eig(a)
-#matrix multiplication 
+#matrix multiplication
 np.dot(a,b)
+
+#NOTE 1 -
+Example 1-
+The frequency table of words in four sample emails is shown below. The angle between emails 1 and 2 is (the first two rows):
+
+money	hurry	meeting	powerpoint
+2   	1	     0	      0
+0	    0	     1	      1
+1	    1	     0	      0
+1	    1	     1	      0
+
+import numpy as np
+e1 = np.array([2,1,0,0])
+e2 = np.array([0,0,1,1])
+e3 = np.array([1,1,1,0])
+dot_prod = np.dot(e1,e2)
+
+#NOTE 2 -
+Angle Between Emails
+The frequency table of words in four sample emails is shown below. You want to know whether email number 4 (the last row) is more similar to email 1 or email 3. Choose all the correct options.
+
+money	hurry	meeting	powerpoint
+2	1	0	0
+0	0	1	1
+1	1	0	0
+1	1	1	0
+
+import numpy as np
+e1 = np.array([2,1,0,0])
+e2 = np.array([0,0,1,1])
+e3 = np.array([1,1,0,0])
+e4 = np.array([1,1,1,0])
+dot_prod = np.dot(e1,e2)
+dot_prod_1_4 = np.dot(e1,e4)
+dot_prod_3_4 = np.dot(e3,e4)
+ang_1_4 = np.arccos(dot_prod_1_4/(np.sqrt(5)*np.sqrt(3)))*(180/np.pi)
+ang_3_4 = np.arccos(dot_prod_3_4/(np.sqrt(2)*np.sqrt(3)))*(180/np.pi)

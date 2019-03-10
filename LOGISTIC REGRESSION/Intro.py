2b37a90dec2c5a61d9c64cca55578f53543e7273
Samrat Kar <samratk@gmail.com>
2019-03-10 19:54:30 +0530

    UPDATED

---
+++ LOGISTIC REGRESSION/Intro.py
@@ -0,0 +1,7 @@
+from scipy.special import expit
+#This is the sigmomid function
+expit (2)
+
+For the sigmoid curve (β0 = -15 and β1 = 0.065), what will be the probability of diabetes for a patient with sugar level 240?
+
+expit (-15 + 0.065*240)
